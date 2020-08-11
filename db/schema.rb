e45b744c7735a59e36a3e07742602f340b08b675
araki174 <arakiwork484@gmail.com>
2020-08-11 18:16:14 +0900

    メッセージ送信機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,13 @@
 ActiveRecord::Schema.define(version: 2020_08_11_063155) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "room_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -45,4 +50,6 @@ ActiveRecord::Schema.define(version: 2020_08_11_063155) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
