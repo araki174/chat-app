9a7a891d6a444b79861a31f4cb741733117d0059
araki174 <arakiwork484@gmail.com>
2020-08-12 14:53:48 +0900

    各モデルのテスト

---
+++ spec/models/room_spec.rb
@@ -0,0 +1,19 @@
+
+RSpec.describe Room, type: :model do
+  describe '#create' do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it "nameの値が存在すれば登録できること" do
+      expect(@room).to be_valid
+
+    end
+    it "nameが空では登録できないこと" do
+      @room.name = nill
+      @romm.valid?
+      expect(@room.error.full_messages).to include("Name can't be blank")
+
+    end
+  end
+end
